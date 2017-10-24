fb9e88af69989d1375093b67af5bb460fbb8f13e
Yarlaxle <adaswudzinski94@gmail.com>
2017-10-24 21:53:52 +0200

    Simple protocol implementation with const values

--- lab1/src/main/java/Client.java
+++ lab1/src/main/java/Client.java
@@ -1,23 +1,39 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.json.simple.JSONObject;
+
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Socket socket = new Socket("localhost", 6789);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
+        JSONObject req1 = new JSONObject();
+        req1.put("request", "keys");
+        out.writeObject(req1);
+
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        JSONObject response1 = (JSONObject) in.readObject();
+        System.out.println("Received: " + response1.toJSONString());
+
+        JSONObject request2 = new JSONObject();
+        request2.put("a", "123");
+        out.writeObject(request2);
+
+        JSONObject response2 = (JSONObject) in.readObject();
+        System.out.println("Received: " + response2.toJSONString());
+
+        JSONObject request3 = new JSONObject();
+        request3.put("encryption", "none");
+        out.writeObject(request3);
+
+        JSONObject request4 = new JSONObject();
+        request4.put("msg", "witam");
+        request4.put("from", "John");
+        out.writeObject(request4);
+
+
+        socket.close();
     }
 }
--- lab1/src/main/java/Server.java
+++ lab1/src/main/java/Server.java
@@ -1,26 +1,47 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.json.simple.JSONObject;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        String clientSentence;
-        String capitalizedSentence;
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         ServerSocket welcomeSocket = new ServerSocket(6789);
 
         while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+            Socket socket = welcomeSocket.accept();
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            JSONObject request1 = (JSONObject) in.readObject();
+            System.out.println("Received: " + request1.toJSONString());
+
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+            JSONObject response1 = new JSONObject();
+            response1.put("p", "123");
+            response1.put("g", "123");
+            out.writeObject(response1);
+
+            JSONObject request2 = (JSONObject) in.readObject();
+            System.out.println("Received: " + request2.toJSONString());
+
+            JSONObject response2 = new JSONObject();
+            response2.put("b", "123");
+            out.writeObject(response2);
+
+           JSONObject request3 = (JSONObject) in.readObject();
+            System.out.println("Received: " + request3.toJSONString());
+            //done
+
+
+            JSONObject msg = (JSONObject) in.readObject();
+            System.out.println("Received: " + msg.toJSONString());
+
+
+
+
         }
     }
 }
