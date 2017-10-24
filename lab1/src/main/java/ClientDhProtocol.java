25391507f2e2bb030119433d478fe11bf962fb1e
Yarlaxle <adaswudzinski94@gmail.com>
2017-10-25 00:34:21 +0200

    Added validation to protocol

--- lab1/src/main/java/ClientDhProtocol.java
+++ lab1/src/main/java/ClientDhProtocol.java
@@ -1,6 +1,7 @@
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -10,25 +11,47 @@ public class ClientDhProtocol {
         Socket socket = new Socket(host, port);
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
-        JSONObject req1 = new JSONObject();
-        req1.put("request", "keys");
-        out.writeObject(req1);
+        JSONObject initToServer = new JSONObject();
+        initToServer.put("request", "keys");
+        out.writeObject(initToServer);
 
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        JSONObject response1 = (JSONObject) in.readObject();
-        System.out.println("Received: " + response1.toJSONString());
 
-        JSONObject request2 = new JSONObject();
-        request2.put("a", "123");
-        out.writeObject(request2);
+        JSONObject pgFromServer = (JSONObject) in.readObject();
+        if(!validPGRequest(pgFromServer)) throw new InvalidObjectException("Invalid P and G");
 
-        JSONObject response2 = (JSONObject) in.readObject();
-        System.out.println("Received: " + response2.toJSONString());
+        JSONObject secretToServer = new JSONObject();
+        secretToServer.put("a", "123");
+        out.writeObject(secretToServer);
 
-        JSONObject request3 = new JSONObject();
-        request3.put("encryption", "none");
-        out.writeObject(request3);
+        JSONObject secretFromServer = (JSONObject) in.readObject();
+        if(!validSecretRequest(secretFromServer)) throw new InvalidObjectException("Invalid secrect B");
+
+        JSONObject encryptionToServer = new JSONObject();
+        encryptionToServer.put("encryption", "none");
+        out.writeObject(encryptionToServer);
 
         return new Connection(in, out, socket);
+    }
+
+    private static boolean validPGRequest(JSONObject request) {
+        return request.containsKey("p") && isInteger((String) request.get("p"))
+                && request.containsKey("g") &&  isInteger((String) request.get("g"));
+    }
+
+    private static boolean validSecretRequest(JSONObject request) {
+        return request.containsKey("b") && isInteger((String)request.get("b"));
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        // only got here if we didn't return false
+        return true;
     }
 }
--- lab1/src/main/java/ServerDhProtocol.java
+++ lab1/src/main/java/ServerDhProtocol.java
@@ -1,37 +1,72 @@
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerDhProtocol {
+    private static final String P = "23";
+    private static final String G = "5";
     static Connection setupConnection(ServerSocket welcomeSocket) throws IOException, ClassNotFoundException {
         Socket socket = welcomeSocket.accept();
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
-        JSONObject request1 = (JSONObject) in.readObject();
-        System.out.println("Received: " + request1.toJSONString());
+        JSONObject initFromClient = (JSONObject) in.readObject();
+        if(!validInitRequest(initFromClient)) throw new InvalidObjectException("Invalid init");
 
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        JSONObject pgToClient = new JSONObject();
+        pgToClient.put("p", P);
+        pgToClient.put("g", G);
+        out.writeObject(pgToClient);
 
-        JSONObject response1 = new JSONObject();
-        response1.put("p", "123");
-        response1.put("g", "123");
-        out.writeObject(response1);
+        JSONObject secretFromClient = (JSONObject) in.readObject();
+        if(!validSecretRequest(secretFromClient)) throw new InvalidObjectException("Invalid secrect A");
 
+        JSONObject secretToClient = new JSONObject();
+        secretToClient.put("b", "123");
+        out.writeObject(secretToClient);
 
-        JSONObject request2 = (JSONObject) in.readObject();
-        System.out.println("Received: " + request2.toJSONString());
-
-        JSONObject response2 = new JSONObject();
-        response2.put("b", "123");
-        out.writeObject(response2);
-
-        JSONObject request3 = (JSONObject) in.readObject();
-        System.out.println("Received: " + request3.toJSONString());
+        JSONObject encryptionFromClient = (JSONObject) in.readObject();
+        if(!validEncryptionRequest(encryptionFromClient)) throw new InvalidObjectException("Invalid encryption");
 
         return new Connection(in, out, socket);
+    }
+
+    private static boolean validEncryptionRequest(JSONObject request) {
+        return isEncryptionOmitted(request) || request.containsKey("encryption") && isSupportedEncryption(request);
+    }
+
+    private static boolean isEncryptionOmitted(JSONObject request) {
+        return request.containsKey("msg");
+    }
+
+    private static boolean isSupportedEncryption(JSONObject request) {
+        return request.get("encryption").equals("none")
+                || request.get("encryption").equals("xor")
+                || request.get("encryption").equals("cezar");
+    }
+
+    private static boolean validSecretRequest(JSONObject request) {
+        return request.containsKey("a") && isInteger((String)request.get("a"));
+    }
+
+    private static boolean validInitRequest(JSONObject request) {
+        return request.containsKey("request") && request.get("request").equals("keys");
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        // only got here if we didn't return false
+        return true;
     }
 }
